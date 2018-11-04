9c892409e08a8948456be3958f3039271cc6f8ea
Brian Bystrom <bbystrom@uncc.edu>
2018-11-04 16:12:11 +0000

    added remember me option

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ItemsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @item = items(:one)
+    @user = users(:brian)
   end
 
   test "should get index" do
