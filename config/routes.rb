433e74c4c9cf41c55129920eb2805710251c2f72
Brian Bystrom <bbystrom@uncc.edu>
2018-11-09 19:41:56 +0000

    dynamic items on home page and link to individual item

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   resources :rentals
   resources :items
   get     'home/index'
-  get     '/signup',  to: 'users#new'
-  get     '/login',   to: 'sessions#new'
-  post    '/login',   to: 'sessions#create'
-  get     '/logout',  to: 'sessions#destroy'
+  get     '/signup',    to: 'users#new'
+  get     '/login',     to: 'sessions#new'
+  post    '/login',     to: 'sessions#create'
+  get     '/logout',    to: 'sessions#destroy'
+  get     '/items/:id', to: 'items#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
