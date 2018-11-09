54715fe3ff772e3b70d223102221343a30ed5dec
Brian Bystrom <bbystrom@uncc.edu>
2018-11-09 16:47:02 +0000

    Added a seed file to fill in the database.  Use rails db:migrate then rails db:seed to fill in the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |n|
+  email = "example-#{n+1}@example.com"
+  password = "password"
+  User.create!( fname:  "Joe #{n}",
+                lname:  "Schmo #{n}",
+                email: email,
+                username: "testuser#{n}",
+                password:              password,
+                password_confirmation: password,
+                status:  true,
+                admin:   false,
+                street:  "9201 University City Blvd",
+                city:    "Charlotte",
+                state:   "NC",
+                zip:     "28223")
+end
+
+10.times do |n|
+    @item = Item.create!(   user_id: 1,
+                    serial_no: "SNO000#{n}",
+                    name: "Item #{n}",
+                    tag: "tag1, tag2",
+                    price: "#{n}",
+                    brand: "Brand #{n}",
+                    model: "Model #{n}")
+end
