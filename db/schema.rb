3374c0501e5d2189c91498ed1ff7b558af5a8811
Brian Bystrom <bbystrom@uncc.edu>
2018-11-09 20:56:16 +0000

    added description to item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_235348) do
+ActiveRecord::Schema.define(version: 2018_11_09_205527) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_235348) do
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "rentals", force: :cascade do |t|
