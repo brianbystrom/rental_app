68966a96d518495bbd58ad83060de53fc35629ca
Brian Bystrom <bbystrom@uncc.edu>
2018-11-13 01:25:03 +0000

    removed checked out items from the main page as items available to rent.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.where.not(:id => Rental.where(buyer_checkin_confirm: false).or(Rental.where(seller_checkin_confirm: false)))
+
+    #@rentals = Rental.where(user_id: params[:id]).where(buyer_checkin_confirm: false).or(Rental.where(seller_checkin_confirm: false))
+    
   end
 end
