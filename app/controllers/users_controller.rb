4935699783ca35712c3083ac658d69db81d63ca0
Brian Bystrom <bbystrom@uncc.edu>
2018-12-04 23:35:14 +0000

    added error handling to send the page back to hom when the record doesn't exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,50 +11,54 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     
-    @user = User.find(params[:id])
-    @rentals = @user.rentals.where(buyer_checkin_confirm: false).where(seller_checkin_confirm: false)
-    @pending_items = Rental.where(item_id: @user.items).where(approval: false)
-    puts "RENTALS: " + @rentals.count.to_s
-    puts "PENDING: " + @pending_items.count.to_s
-    @rental_buyer_history = Rental.where(user_id: params[:id]).where(buyer_checkin_confirm: true).where(seller_checkin_confirm: true)
-    @rental_seller_history = Rental.where(item_id: @user.items).where(buyer_checkin_confirm: true).where(seller_checkin_confirm: true)
-    #@rental_history = @rental_buyer_history + @rental_seller_history
-    puts "BUYER HISTORY: " + @rental_buyer_history.count.to_s
-    puts @rental_buyer_history
-    puts "SELLER HISTORY: " + @rental_seller_history.count.to_s
-    puts @rental_seller_history
-    #@ip_addr = request.env[‘REMOTE_ADDR’]
-    
-    @comments = Comment.where(commented_id: params[:id])
-    
-    @rating_sum = 0
-    @rating_count = 0
-    
-    if @rental_buyer_history.count > 0
-      @rental_buyer_history.each do |brental|
-        if brental.seller_rating
-          @rating_sum = @rating_sum + brental.seller_rating
-          @rating_count = @rating_count + 1
-        end
-      end
-    end
-    
-    if @rental_seller_history.count > 0
-      @rental_seller_history.each do |srental|
-        if srental.buyer_rating
-          @rating_sum = @rating_sum + srental.buyer_rating
-          @rating_count = @rating_count + 1
-        end
-      end
-    end
-    
-    puts "RATING SUM: " + @rating_sum.to_s
-    puts "RATING COUNT: " + @rating_count.to_s
-    
-    if @rating_count > 0
-      @rating = (@rating_sum / @rating_count).ceil
+    if !@user
+      redirect_to root_path
     else
-      @rating = 0
+      @user = User.find(params[:id])
+      @rentals = @user.rentals.where(buyer_checkin_confirm: false).where(seller_checkin_confirm: false)
+      @pending_items = Rental.where(item_id: @user.items).where(approval: false)
+      puts "RENTALS: " + @rentals.count.to_s
+      puts "PENDING: " + @pending_items.count.to_s
+      @rental_buyer_history = Rental.where(user_id: params[:id]).where(buyer_checkin_confirm: true).where(seller_checkin_confirm: true)
+      @rental_seller_history = Rental.where(item_id: @user.items).where(buyer_checkin_confirm: true).where(seller_checkin_confirm: true)
+      #@rental_history = @rental_buyer_history + @rental_seller_history
+      puts "BUYER HISTORY: " + @rental_buyer_history.count.to_s
+      puts @rental_buyer_history
+      puts "SELLER HISTORY: " + @rental_seller_history.count.to_s
+      puts @rental_seller_history
+      #@ip_addr = request.env[‘REMOTE_ADDR’]
+      
+      @comments = Comment.where(commented_id: params[:id])
+      
+      @rating_sum = 0
+      @rating_count = 0
+      
+      if @rental_buyer_history.count > 0
+        @rental_buyer_history.each do |brental|
+          if brental.seller_rating
+            @rating_sum = @rating_sum + brental.seller_rating
+            @rating_count = @rating_count + 1
+          end
+        end
+      end
+      
+      if @rental_seller_history.count > 0
+        @rental_seller_history.each do |srental|
+          if srental.buyer_rating
+            @rating_sum = @rating_sum + srental.buyer_rating
+            @rating_count = @rating_count + 1
+          end
+        end
+      end
+      
+      puts "RATING SUM: " + @rating_sum.to_s
+      puts "RATING COUNT: " + @rating_count.to_s
+      
+      if @rating_count > 0
+        @rating = (@rating_sum / @rating_count).ceil
+      else
+        @rating = 0
+      end
     end
   end
 
@@ -114,7 +118,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find_by_id(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
